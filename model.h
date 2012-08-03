2d4243a4ec6c9f2416ede05e6c132ea30a023ef6
Hao-Che Kao <kao50126@gmail.com>
2012-08-03 17:43:15 +0800

    Add new file to implement hemi-cube Form Factor caculation

---
+++ model.h
@@ -0,0 +1,26 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+/// Vertex
+typedef struct vertex{
+
+    double x;
+    double y;
+    double z;
+
+    struct vertex* next;
+
+} VERTEX;
+
+typedef struct mesh
+
+/// Surface
+typedef struct surface{
+
+
+    VERTEX** vertex;
+
+
+
+
+}
